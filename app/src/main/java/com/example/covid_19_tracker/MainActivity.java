d5e8caab71360b5ed57ecef664d9e96547d25916
Joe Gerges <jag07@mail.aub.edu>
2020-11-15 18:53:45 +0200

    pannel to add patients

--- app/src/main/java/com/example/covid_19_tracker/MainActivity.java
+++ app/src/main/java/com/example/covid_19_tracker/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         myDatabaseHelper = new DatabaseHelper(this);
 
         addPatient.setOnClickListener(new View.OnClickListener() {
